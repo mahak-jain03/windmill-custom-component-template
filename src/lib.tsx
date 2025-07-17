dab89dc10e44ce48c22646a1e48820580cae5025
Mahak <mahak.jain@grofers.com>
2025-07-17 13:30:16 +0530

    component for page 3

--- src/lib.tsx
+++ src/lib.tsx
@@ -3,15 +3,26 @@ import ReactDOM from "react-dom/client";
 import Component from "./component/Component.tsx";
 // import Component from "./component/MinimalComponent.tsx";
 
-import { type WindmillProps } from "./global";
+// import { type WindmillProps } from "./global";
 
-export function customComponent(props: WindmillProps<any>) {
+
+type WindmillProps = {
+  id: string;
+  outputs: any;
+  passSetters: (setter: any) => void;
+  setOutput: (output: any) => void;
+  renderInit: boolean;
+  render?: boolean; // Add this line if needed
+};
+
+export function customComponent(props: WindmillProps) {
   ReactDOM.createRoot(document.getElementById(props.id)!).render(
     <React.StrictMode>
       <Component
+        outputs={props.outputs}
         passSetters={props.passSetters}
         setOutput={props.setOutput}
-        renderInit={props.render}
+        renderInit={props.renderInit}
       />
     </React.StrictMode>
   );
@@ -21,4 +32,4 @@ if (window.windmill === undefined) {
   window.windmill = {};
 }
 
-window.windmill[__COMPONENT_NAME__] = customComponent;
+ window.windmill[__COMPONENT_NAME__] = customComponent;
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
-const __COMPONENT_NAME__ = "My Component";
+const __COMPONENT_NAME__ = "CustomComponent";
 
 export default defineConfig(({ mode }) => ({
   plugins: [
